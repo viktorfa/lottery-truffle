99076358e5e48cd76b88950df0a593cd657f6288
viktorfa <vikfand@gmail.com>
2019-06-01 10:35:41 +0200

    Update LotteryMatch to use default winner.

--- contracts/LotteryMatch.sol
+++ contracts/LotteryMatch.sol
@@ -118,7 +118,11 @@ contract LotteryMatch {
         } else if (alice == address(0) && bob != address(0)) {
             return bob;
         } else if (alice == address(0) && bob == address(0)) {
-            return alice;
+            if (isFirstLevel) {
+                return lottery.getPlayer(index * 2);
+            } else {
+                left.getWinner();
+            }
         }
         
         // Check if parties have made commitments.
@@ -127,7 +131,11 @@ contract LotteryMatch {
         } else if (commitments[alice] == 0 && commitments[bob] != 0) {
             return bob;
         } else if (commitments[alice] == 0 && commitments[bob] == 0) {
-            return alice;
+            if (isFirstLevel) {
+                return lottery.getPlayer(index * 2);
+            } else {
+                left.getWinner();
+            }
         }
 
         // Check if parties have revealed.
@@ -136,7 +144,11 @@ contract LotteryMatch {
         } else if (secrets[alice] == 0 && secrets[bob] != 0) {
             return bob;
         } else if (secrets[alice] == 0 && secrets[bob] == 0) {
-            return alice;
+            if (isFirstLevel) {
+                return lottery.getPlayer(index * 2);
+            } else {
+                left.getWinner();
+            }
         }
         
         // Both parties have revealed, let's toss the coin.
